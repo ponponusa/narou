5c3509bdb35242768ed3ecbcfc1b9647326fc5f7
ponponusa <ponpon@usa>
2025-11-04 15:53:16 +0900

    - 旧narou互換エントリポイントを削除 - CIとRuboCop設定をnarou-mod用に更新 - Gemfileコメントを最新化

--- narou.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM -*- mode: ruby -*-
-echo narou コマンドは narou-mod に名称変更されました。narou-mod.bat をご利用ください。
-@if exist "narou-mod.bat" (
-	call narou-mod.bat %*
-) else (
-	ruby -e "warn 'narou-mod.bat が見つかりません'; exit 1"
-	exit /b 1
-)
