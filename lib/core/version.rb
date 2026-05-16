ed5cbe605b31be7d9db6f05cdc9683f80d0f9dd3
ponpon.USA <init0531.usa@gmail.com>
2026-05-16 16:03:11 +0900

    fix: 3つのバグ修正と v3.1.4 リリース
    - fix: Issue #105 - remove コマンドの NameError を修正
      remove.rb で Downloader クラスが未ロードだった問題を修正
    
    - fix: Issue #103 - カクヨム・ハーメルンでタグが一部しか取得できない問題を修正
      複数マッチへの対応として sitesetting.rb の multi_match メソッドを改善
      tags キーに複数のタグがマッチする場合、配列として格納するように修正
      database_updater.rb でタグ配列に対応
    
    - fix: Issue #102 - web コマンドで --port オプションが無視される問題を修正
      外部ループで argv が処理されるときにオプションが失われていたため
      @options["port"] がある場合、再度 argv に追加するように修正
    
    - chore: バージョンを 3.1.4 に更新
    
    @ponponusa

--- lib/core/version.rb
+++ lib/core/version.rb
@@ -5,7 +5,7 @@
 #
 
 module Narou
-  VERSION = "3.1.3".freeze
+  VERSION = "3.1.4".freeze
 
   commit_path = File.expand_path("../../commitversion", __dir__)
   commit_value = if File.exist?(commit_path)
