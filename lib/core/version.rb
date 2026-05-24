d6c08f0ba5812c74b64a01df4ed6432819d94158
ponpon.USA <init0531.usa@gmail.com>
2026-05-24 13:04:18 +0900

    バージョンを 3.1.5 に更新

--- lib/core/version.rb
+++ lib/core/version.rb
@@ -5,7 +5,7 @@
 #
 
 module Narou
-  VERSION = "3.1.4".freeze
+  VERSION = "3.1.5".freeze
 
   commit_path = File.expand_path("../../commitversion", __dir__)
   commit_value = if File.exist?(commit_path)
